12b4cc9e3b14db7d1715383eba445845ceacefff
Altynay-ayyao <77488933+Altynay-ayyao@users.noreply.github.com>
2022-06-29 09:34:20 +0300

    image added

---
+++ script.js
@@ -0,0 +1,8 @@
+//Slideshow
+const slideshowDivs = () => {
+  for (let i = 1; i <= 5; i++) {
+    const div = document.createElement('div');
+    div.style.backgroundImage = ``;
+  }
+};
+//End of Slideshow
