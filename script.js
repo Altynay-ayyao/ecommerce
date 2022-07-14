c1c43e2a10399f04c9cb8e5df365ae5e6ce2daf2
Altynay-ayyao <77488933+Altynay-ayyao@users.noreply.github.com>
2022-07-14 12:58:59 +0300

    back to top button added, about me page styled

--- script.js
+++ script.js
@@ -33,3 +33,25 @@ const slideshow = () => {
 slideshow();
 
 //End of Slideshow
+
+//Get the bak to top button:
+mybutton = document.getElementById('myBtn');
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//end of scroll to top button
